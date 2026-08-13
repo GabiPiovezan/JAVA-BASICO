78461aac678525f10edd7c717653f44fa155bfde
gabriella62318596 <gabymarqueserosa@gmail.com>
2026-08-13 19:05:59 -0300

    exercicio17

---
+++ src/aula17/Exercicio2.java
@@ -0,0 +1,11 @@
+package aula17;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
