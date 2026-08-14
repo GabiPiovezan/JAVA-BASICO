a0aaca3e983beb8382687a33b1f370934534fa7b
gabriella Piovezan <gabymarqueserosa@gmail.com>
2026-08-14 17:10:55 -0300

    Exercicios

--- src/aula17/Exercicio2.java
+++ src/aula17/Exercicio2.java
@@ -7,5 +7,15 @@ public class Exercicio2 {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite seu nome de Usuário: ");
+        String nomeUsuario = scanner.nextLine();
+        System.out.println("Digite a sua senha: ");
+        String senhaUsuario = scanner.nextLine();
+
+        if (nomeUsuario.equals(senhaUsuario)){
+            System.out.println("Erro: A senha e o nome de usuário não podem ser iguais.");
+        } else {
+            System.out.println("Cadastro Aprovado!");
+        }
     }
 }
---
+++ src/aula17/Exercicio6.java
@@ -0,0 +1,15 @@
+package aula17;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int numero = 0; numero <21; numero++){
+            System.out.println(numero);
+        }
+    }
+}
